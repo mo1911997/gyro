7325dc706984a63746819cc05275c525f2f19cc7
Mohit Dixit <mmd26.aka@gmail.com>
2019-05-18 15:05:18 +0530

    Edited serializers.py from UserModel

---
+++ sample/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import Users
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = "__all__"
