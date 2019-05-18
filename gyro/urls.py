3fc731565e70dcff1c419d26811de3212758c0cc
Mohit Dixit <mmd26.aka@gmail.com>
2019-05-18 15:28:50 +0530

    changed urls.py in sample app

--- gyro/urls.py
+++ gyro/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/',include('sample.urls'))
+    path('users/',include('urls.sample'))
 ]
