c626aa6dfb7b393e626a4f93406f073f483860e7
Mohit Dixit <mmd26.aka@gmail.com>
2019-05-20 11:12:01 +0530

    Edited urls.py in sample app

--- sample/views.py
+++ sample/views.py
@@ -5,7 +5,7 @@ from .serializers import *
 from rest_framework.response import Response
 # Create your views here.
 class UserView(APIView):
-    def get(self):
+    def get(self, request, format=None):
         users = Users.objects.all()
         serializer = UsersSerializer(users,many=True)
         return Response(serializer.data)
