ba1d9f380bf12d317421fff0a8f4f47ce04c557d
Mohit Dixit <mmd26.aka@gmail.com>
2019-05-20 12:20:54 +0530

    Edited urls.py in sample app

--- sample/urls.py
+++ sample/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path,include
-from .views import UserView
+from .views import UserView,UserAddView
 
 urlpatterns = [
 
-    path('',UserView.as_view())
+    path('',UserView.as_view()),
+    path('adduser/',UserAddView.as_view())
 ]
--- sample/views.py
+++ sample/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import Users
+from rest_framework import status
 from .serializers import *
 from rest_framework.response import Response
 # Create your views here.
@@ -9,3 +10,12 @@ class UserView(APIView):
         users = Users.objects.all()
         serializer = UsersSerializer(users,many=True)
         return Response(serializer.data)
+
+class UserAddView(APIView):
+    def post(self, request, format=None):
+
+        serializer = UsersSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
