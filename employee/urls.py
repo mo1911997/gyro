e9efe1254a1dc4364d32f1b09fb33f974720467c
Mohit Dixit <mmd26.aka@gmail.com>
2019-05-23 12:39:31 +0530

    changed views.py

--- employee/urls.py
+++ employee/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path,include
-from .views import EmployeeView,GetSalView
+from .views import EmployeeView,GetSalView,EmployeeAddView
 
 urlpatterns = [
 
     path('',EmployeeView.as_view()),
-    # path('addemp/',EmployeeAddView.as_view()),
+     path('addemp/',EmployeeAddView.as_view()),
     path('getsal/',GetSalView.as_view())
 ]
