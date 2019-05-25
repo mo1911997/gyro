d645dc00d7b38024bf966f8a721d94357d2d5709
Rahul Gourshettiwar <arenalstone1279@gmail.com>
2019-05-25 11:46:22 +0530

    string to json

--- employee/views.py
+++ employee/views.py
@@ -42,7 +42,7 @@ class GetSalView(APIView):
                  if (j == "NN" or j =="JJ"):
                      salary_param.append(i)
                      #ss = json.load(salary_param)
-            return Response(salary_param)
+            return Response(output)
             # name = request.data['name']
             # something = Employee.objects.filter(name=name).values()
             # myarr = sent_tokenize(sentence)
