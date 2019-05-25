015aea5fdfdfa07d8e9a21a64a0e7d5f4331e2b5
Rahul Gourshettiwar <arenalstone1279@gmail.com>
2019-05-25 11:15:53 +0530

    string to json

--- employee/views.py
+++ employee/views.py
@@ -37,10 +37,10 @@ class GetSalView(APIView):
             sentence = request.data['sentence']
             tokens_tag = pos_tag(word_tokenize(sentence))
             output = ne_chunk(tokens_tag)
-            salary_param = "aaaa"
-            # for i, j in tokens_tag:
-            #     if (j == "NN"):
-            #         salary_param = i
+            #salary_param = "aaaa"
+            for i, j in tokens_tag:
+                 if (j == "NN"):
+                     salary_param = i
                     # ls.append(i)
             ss = json.load(salary_param)
             return Response(ss)
