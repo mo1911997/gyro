ef812cfb375973f0acfb5480df2653411b0aac63
Rahul Gourshettiwar <arenalstone1279@gmail.com>
2019-06-17 13:47:47 +0530

    trace back

--- employee/views.py
+++ employee/views.py
@@ -85,7 +85,7 @@ class MainView(APIView):
             r = requests.post('https://peaceful-shore-77889.herokuapp.com/employee/getsal/', data = request.data)
         elif (flag == 4):
             r = requests.get('https://peaceful-shore-77889.herokuapp.com/employee/getprofileconv/')
-            if (r.content == "thank you"):
+            if (r.__str__() == "thank you"):
                 return Response("in here")
                 d = {}
                 d['name'] = list1[1]
