06f35bd1201ab704abe0529d9afc4210bcb565f9
Rahul Gourshettiwar <arenalstone1279@gmail.com>
2019-06-28 16:43:04 +0530

    changes in views.py

--- employee/views.py
+++ employee/views.py
@@ -144,7 +144,7 @@ class LeaveApply(APIView):
         #     return Response("thank you")
         users = LeaveConverseResponses.objects.all()
         serializer = LeaveConSerializer(users, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data['sentence'])
 
 class ProfileApply(APIView):
 
